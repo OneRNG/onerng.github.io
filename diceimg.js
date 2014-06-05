e8a60c956b2801e7feef0db2f1cedd8be89e76c3
Jim Cheetham <jim@gonzul.net>
2014-06-05 22:20:07 +1200

    Added random-order dice images via javascript

---
+++ diceimg.js
@@ -0,0 +1,30 @@
+// diceimg.js - display 6 die images in a random order
+
+// Pass an array, returns an array
+// * Inspired by https://github.com/coolaj86/knuth-shuffle
+function shuffle(a) {
+
+        // Step through the array, index value "i"
+        // Pick a 'random' element "ri"
+        // Swap the two, using "t" to hold one of the values
+        var i = a.length, t, ri;
+        while (0 != i) {
+                ri = Math.floor(Math.random() * i);
+                i -= 1;
+                t = a[i];
+                a[i] = a[ri];
+                a[ri] = t;
+        }
+        return a;
+}
+
+// The dice array, using the icon names as values
+var dice = shuffle([10541, 10542, 10543, 10544, 10545, 27626]);
+
+// Output the dice links as per the new array ...
+var len = dice.length;
+for (var i=0; i<len; ++i) {
+        // The img filenames are ugly ...
+        document.write ("<img src='img/icon_",(dice[i]),"/icon_",(dice[i]),".svg'/>");
+}
+
